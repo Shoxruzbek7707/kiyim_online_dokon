8e15a8543e6962a6b340ba0fabfd38a60b15ce99
Shoxruzbek7707 <shoxruzqoshoqboyev778@gmail.com>
2025-11-16 21:00:13 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/kiyim_online_dokon/KiyimOnlineDokonApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.kiyim_online_dokon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiyimOnlineDokonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiyimOnlineDokonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/kiyim_online_dokon/KiyimOnlineDokonApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.kiyim_online_dokon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiyimOnlineDokonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
