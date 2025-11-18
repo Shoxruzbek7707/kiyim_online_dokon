baf72db895e31724b20b307ad268f2db6c278fe7
user <sherzodjovliyev16@gmail.com>
2025-11-18 21:36:02 +0500

    Entity

---
+++ src/main/java/uz/pdp/kiyim_online_dokon/entity/OrderItem.java
@@ -0,0 +1,28 @@
+package uz.pdp.kiyim_online_dokon.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name ="order_items" )
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne @JoinColumn(nullable = false)
+    private Orders order;
+
+    @ManyToOne @JoinColumn(nullable = false)
+    private Products product;
+
+    private Double priceAtPurchase;
+    private Integer quantity;
+}
--- src/main/java/uz/pdp/kiyim_online_dokon/entity/Orders.java
+++ src/main/java/uz/pdp/kiyim_online_dokon/entity/Orders.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
