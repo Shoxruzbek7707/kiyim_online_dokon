16656f3709a434450b82fe71b79967fd03b0e359
user <sherzodjovliyev16@gmail.com>
2025-11-27 21:17:59 +0500

    swagger add(Tekshiring Shaxruz)

---
+++ src/main/java/uz/pdp/kiyim_online_dokon/controller/UserController.java
@@ -0,0 +1,34 @@
+package uz.pdp.kiyim_online_dokon.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uz.pdp.kiyim_online_dokon.service.impl.UsersServiceImpl;
+import uz.pdp.kiyim_online_dokon.service.interfaces.UsersService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@Tag(name = "User API", description = "Operations related to users")
+public class UserController {
+
+    @Autowired
+    UsersServiceImpl userServiceImpl;
+
+    @Operation(summary = "Get all users", description = "Returns a list of all users")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "404", description = "Users not found")
+    })
+    @GetMapping
+    public <UserDto>
+    List<UserDto> getAll() {
+        return (List<UserDto>) userServiceImpl.getAllUsers();
+    }
+}
